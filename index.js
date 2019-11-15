ed480eedc1145b5772620eddcfa3f92b91a8fac0
Jon Ursenbach <erunion@users.noreply.github.com>
2019-11-14 19:46:34 -0800

    feat: expanding our ruleset to include expanded checks (#1)

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ module.exports = {
   extends: [
     "airbnb-base",
     "eslint:recommended",
+    "plugin:eslint-comments/recommended",
     "plugin:import/errors",
     "plugin:import/warnings",
     "prettier"
   ],
+  plugins: ["node"],
   rules: {
     "arrow-body-style": "off",
+
+    "eslint-comments/disable-enable-pair": ["error", {"allowWholeFile": true}],
+
     "func-names": "off",
 
     "import/default": "off",
@@ -16,6 +21,10 @@ module.exports = {
 
     // Disallow shadowing of any variable that isn't "err" as this is a common case that is acceptable
     "no-shadow": ["error", {"allow": ["err"]}],
+
+    "node/no-deprecated-api": "error",
+    "node/no-exports-assign": "error",
+    "node/no-extraneous-require": "error",
 
     "prefer-destructuring": "off",
   }
