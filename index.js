70209b070bc50e71599888bf05960e11cbbb28c2
Jon Ursenbach <erunion@users.noreply.github.com>
2019-11-20 11:40:16 -0800

    feat: a new react standards config (#7)

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ module.exports = {
     "import/named": "off", // @fixme Disabling this for now, but there are some weird callouts with it.
     "import/order": "off",
 
+    "no-else-return": ["error", { allowElseIf: true }],
+
     // Disallow shadowing of any variable that isn't "err" as this is a common case that is acceptable
     "no-shadow": ["error", { allow: ["err"] }],
 
