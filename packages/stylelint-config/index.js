e68116ec2d7c935f7ad8b09d798a69811320ce49
Daniels Lee <daniels@readme.io>
2022-03-25 20:52:04 -0700

    wip: add more rules and conventions based on our current codebase

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -6,11 +6,40 @@ module.exports = {
     // Standard rules for SCSS
     'stylelint-config-standard-scss',
 
-    // Shared config for CSS modules syntax
-    'stylelint-config-css-modules',
+    // Stylelint config inspired by https://sass-guidelin.es
+    'stylelint-config-sass-guidelines',
 
-    // Enable running prettier config as additional rules
+    // Enable prettier formatting for SCSS/CSS
     'stylelint-config-prettier',
     'stylelint-prettier/recommended',
   ],
+  rules: {
+    // Rules not handled by pretty printers
+    'comment-whitespace-inside': 'always',
+    'function-name-case': 'lower',
+    'selector-type-case': 'lower',
+    'value-keyword-case': 'lower',
+
+    // Conflicting stylelint-config-sass-guidelines rules with readme styles
+    'color-named': null,
+    'max-nesting-depth': null,
+
+    // Custom regex of our current BEM selector class pattern
+    'selector-class-pattern': [
+      '^[a-zA-Z0-9]+(-[a-zA-Z0-9]+)*(_[a-zA-Z0-9]+)*$',
+      {
+        message:
+          'Selector should start in CapitalCase or camelCase and optionally followed by kebab-lowercase with hyphens and end with snake_lowercase with underscores (e.g. BlockName-element-name_modifier-name)',
+      },
+    ],
+
+    // Scss rule should be used instead of Stylelint's function-no-unknown.
+    'function-no-unknown': null,
+    'scss/function-no-unknown': true,
+
+    // TODO: Remove this when migrating to Dart Sass
+    // Disallows the use of global function names, as these global functions are
+    // now located inside built-in Sass modules.
+    'scss/no-global-function-names': null,
+  },
 };
