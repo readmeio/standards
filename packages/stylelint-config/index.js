8ef0e90f065d571659dc5dc1df08aaf8d0d34498
Daniels Lee <daniels@readme.io>
2022-04-04 13:14:33 -0700

    docs: update readme

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -23,15 +23,15 @@ module.exports = {
     // ReadMe breaks this rule in many places.
     'max-nesting-depth': null,
 
-    // ReadMe still relies on "input[type='value']" selectors in too many
-    // places. Eventually, it would probably be beneficial to turn this on.
+    // ReadMe relies on "element[attr='value']" selectors in too many places.
+    // Eventually, it may be beneficial to turn this on.
     'selector-no-qualifying-type': null,
 
     // ReadMe relies on legacy color functions (e.g. rgba(0, 0, 0, 0.5))
     // everywhere in addition to Scss allowing this to be written with a color
-    // name (e.g. rgba(black, 0.5)). We may eventually want to consider moving
-    // to "modern" style with comma-free syntax (e.g. rgb(0 0 0 / 0.5)). But
-    // this won't be trivial.
+    // name (e.g. rgba(black, 0.5)). Eventually, consider removing this rule to
+    // use "modern" style with comma-free syntax (e.g. rgb(0 0 0 / 0.5)). This
+    // change however won't be trivial.
     'color-function-notation': 'legacy',
 
     // Custom regex of ReadMe's current BEM selector class pattern.
@@ -42,10 +42,6 @@ module.exports = {
           'Selector should start in CapitalCase or camelCase and optionally followed by either kebab-lowercase with hyphens or snake_lowercase with underscores (e.g. BlockName-element-name_modifier-name)',
       },
     ],
-
-    // Scss rule should be used instead of Stylelint's function-no-unknown.
-    'function-no-unknown': null,
-    'scss/function-no-unknown': true,
 
     // TODO: Remove this when migrating to Dart Sass.
     // Disallows the use of global function names, as these global functions are
