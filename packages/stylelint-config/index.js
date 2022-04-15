61d6e374e2bfa13bae8520ab0e32a6bb2e4fb083
Daniels Lee <daniels@readme.io>
2022-04-15 16:02:21 -0700

    refactor: alphabetize custom rule overrides

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -17,19 +17,6 @@ module.exports = {
     'stylelint-prettier/recommended',
   ],
   rules: {
-    // ReadMe still uses color names as values in many places.
-    'color-named': null,
-
-    // ReadMe breaks this rule in many places.
-    'max-nesting-depth': null,
-
-    // ReadMe still references elements by #id. Allow at most one per selector.
-    'selector-max-id': 1,
-
-    // ReadMe relies on "element[attr='value']" selectors in too many places.
-    // Eventually, it may be beneficial to turn this on.
-    'selector-no-qualifying-type': null,
-
     // ReadMe relies on legacy color functions (e.g. rgba(0, 0, 0, 0.5))
     // everywhere in addition to Scss allowing this to be written with a color
     // name (e.g. rgba(black, 0.5)). Eventually, consider removing this rule to
@@ -37,13 +24,11 @@ module.exports = {
     // change however won't be trivial.
     'color-function-notation': 'legacy',
 
-    // Custom regex for ReadMe id patterns.
-    'selector-id-pattern': [
-      '^(([a-z][a-z0-9]*(-[a-z0-9]+)*)|([A-Z][a-z0-9]*)+)$',
-      {
-        message: 'Expected id selector to be kebab-case or TitleCase',
-      },
-    ],
+    // ReadMe still uses color names as values in many places.
+    'color-named': null,
+
+    // ReadMe breaks this rule in many places.
+    'max-nesting-depth': null,
 
     // Custom regex of ReadMe's current BEM selector class pattern.
     'selector-class-pattern': [
@@ -53,6 +38,21 @@ module.exports = {
           'Selector should start in CapitalCase or camelCase and optionally followed by either kebab-lowercase with hyphens or snake_lowercase with underscores (e.g. BlockName-element-name_modifier-name)',
       },
     ],
+
+    // Custom regex for ReadMe id patterns.
+    'selector-id-pattern': [
+      '^(([a-z][a-z0-9]*(-[a-z0-9]+)*)|([A-Z][a-z0-9]*)+)$',
+      {
+        message: 'Expected id selector to be kebab-case or TitleCase',
+      },
+    ],
+
+    // ReadMe still references elements by #id. Allow at most one per selector.
+    'selector-max-id': 1,
+
+    // ReadMe relies on "element[attr='value']" selectors in too many places.
+    // Eventually, it may be beneficial to turn this on.
+    'selector-no-qualifying-type': null,
 
     // TODO: Remove this when migrating to Dart Sass.
     // Disallows the use of global function names, as these global functions are
