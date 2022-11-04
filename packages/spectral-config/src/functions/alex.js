98ca0ae2ef82c11184d98538e2e25bef19578fbf
Jon Ursenbach <jon@ursenba.ch>
2022-11-04 10:28:22 -0700

    fix: confusing logic

--- packages/spectral-config/src/functions/alex.js
+++ packages/spectral-config/src/functions/alex.js
@@ -8,11 +8,8 @@ const { text } = require('alex');
  */
 module.exports = function alex(input, options, context) {
   const errors = text(input, { profanitySureness: 1 }).messages;
-  if (!errors.length) {
-    return [];
-  }
 
-  return Object.values(errors)
+  return errors
     .map(err => {
       // We use `invalid` in some response error component descriptions and within the context that
       // they're used they're fine.
