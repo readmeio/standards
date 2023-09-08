a34fcfea89f0a77a283a45f9234c53301f4d215f
Kanad Gupta <git@kanad.dev>
2023-09-08 14:00:07 -0500

    feat: get build working with tsup

---
+++ packages/spectral-config/tsup.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.js'],
+  clean: true,
+  outDir: 'dist',
+  noExternal: ['alex'],
+});
