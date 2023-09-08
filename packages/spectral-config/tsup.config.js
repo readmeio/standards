0f9428ca5126cc26cad511480faa0faab52c76fe
Kanad Gupta <git@kanad.dev>
2023-09-08 15:46:16 -0500

    fix: point the `main` field to the ESM export

--- packages/spectral-config/tsup.config.js
+++ packages/spectral-config/tsup.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   entry: ['src/index.js'],
-  format: ['cjs', 'esm'],
+  format: ['esm'],
   noExternal: ['alex'],
   outDir: 'dist',
 });
