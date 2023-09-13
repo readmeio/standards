31ac955716a0a8bd7ea625ff106cfd3bac19c2a3
Kanad Gupta <8854718+kanadgupta@users.noreply.github.com>
2023-09-13 12:56:24 -0500

    fix(eslint/esm): enable `import/no-commonjs` (#752)

--- packages/eslint-config/esm.js
+++ packages/eslint-config/esm.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['plugin:require-extensions/recommended'],
   plugins: ['require-extensions', 'unicorn'],
   rules: {
+    'import/no-commonjs': 'error',
     'unicorn/prefer-node-protocol': 'error',
   },
 };
