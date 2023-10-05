51e36b8d4cf465ee3d103194ea6ebebe6b9bdbc2
Kanad Gupta <git@kanad.dev>
2023-10-05 11:19:59 -0500

    feat(eslint/esm): enable more ESM rules

--- packages/eslint-config/esm.js
+++ packages/eslint-config/esm.js
@@ -3,7 +3,11 @@ const config = {
   extends: ['plugin:require-extensions/recommended'],
   plugins: ['require-extensions', 'unicorn'],
   rules: {
+    // see here for more rules to possibly enable in the future:
+    // https://gist.github.com/Jaid/164668c0151ae09d2bc81be78a203dd5
     'import/no-commonjs': 'error',
+    'node/no-extraneous-import': 'error',
+    'unicorn/prefer-module': 'error',
     'unicorn/prefer-node-protocol': 'error',
   },
 };
