fb3b4d54a3cd1e358921dc04d2a97d42383af4b6
Jon Ursenbach <jon@ursenba.ch>
2023-10-31 10:48:35 -0700

    feat(eslint): disabling default exports in ESM libraries

--- packages/eslint-config/esm.js
+++ packages/eslint-config/esm.js
@@ -6,6 +6,17 @@ const config = {
     // see here for more rules to possibly enable in the future:
     // https://gist.github.com/Jaid/164668c0151ae09d2bc81be78a203dd5
     'import/no-commonjs': 'error',
+
+    /**
+     * This rule enablement is generally only intended for public-facing libraries but when shipping
+     * a package that exports CJS and ESM exports you cannot do `export default` and also
+     * `export type` in the same file because `export default` gets translated to `export =` in CJS
+     * and TS types can't be layered on top of that.
+     *
+     * @see {@link https://github.com/isaacs/tshy#handling-default-exports}
+     */
+    'import/no-default-export': 'error',
+
     'node/no-extraneous-import': 'error',
     'unicorn/prefer-module': 'error',
     'unicorn/prefer-node-protocol': 'error',
