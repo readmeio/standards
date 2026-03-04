b61e99647d15c69752a4f84adc3d0117638343b2
Tony Li <runnabro@users.noreply.github.com>
2026-03-04 13:14:58 -0800

    remove unnused dep

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -5,7 +5,7 @@ module.exports = [
     rules: { 'import/no-extraneous-dependencies': 'off' },
   },
   {
-    files: ['typescript.js'],
+    files: ['**/typescript.js'],
     rules: {
       // These modules resolve fine at runtime but the import resolver can't trace them.
       'import/no-unresolved': ['error', { ignore: ['@typescript-eslint/eslint-plugin', '@typescript-eslint/parser'] }],
