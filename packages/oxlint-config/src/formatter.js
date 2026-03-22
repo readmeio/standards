a3e24267457cd7f968c898591511715a13cba623
Jon Ursenbach <jon@ursenba.ch>
2026-03-22 16:53:00 -0700

    fix: minor code prettification

--- packages/oxlint-config/src/formatter.js
+++ packages/oxlint-config/src/formatter.js
@@ -6,6 +6,7 @@ export default defineConfig({
   singleQuote: true,
   sortImports: {
     groups: ['type', 'builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
+
     ignoreCase: true,
     'newlines-between': 'always',
     order: 'asc',
